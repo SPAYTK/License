50ee6bef360b1ddda23cb041595c19b2ffa2f9bb
Alberto Sampayo Iglesias <143392770+SPAYTK@users.noreply.github.com>
2025-04-17 17:56:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,49 @@
 
-/*
- * Firmware Básico para Chip Cuántico Híbrido
- * Licencia: MIT
- * Descripción: Control del proceso I/O cuántico-ionico desde microcontrolador
- * Estado del arte - Publicación técnica para validación
- */
-
-#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
 #include <math.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #define PI 3.141592653589793
-#define ENTRADAS 10
+#define ENTRADAS 16
+#define FREQ_BASE_HZ 0.0014
+#define ENERGIA_JULIOS 6.98
 
-int entrada[ENTRADAS] = {1, 0, 1, 1, 1, 0, 1, 1, 1, 1};
-uint8_t salida[ENTRADAS];
+int entrada_digital[ENTRADAS] = {1,0,1,1,0,1,1,0,1,0,1,1,1,0,1,1};
+uint8_t salida_digital[ENTRADAS];
 
-void inicializar_chip() {
-    printf("Inicializando chip cuántico híbrido...\n");
+void uart_init(void) {
+    UBRR0H = 0;
+    UBRR0L = 103;
+    UCSR0B = (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-double excitar_iones(int valor_binario) {
-    double x = (double)valor_binario;
-    return atan(PI) * atan(fabs(-x));
+void uart_transmit(char data) {
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
+}
+
+void uart_print(const char *str) {
+    while (*str) uart_transmit(*str++);
+}
+
+void uart_print_num(uint8_t num) {
+    char buf[4];
+    snprintf(buf, sizeof(buf), "%d", num);
+    uart_print(buf);
+}
+
+void inicializar_chip() {
+    uart_print("Chip Cuantico Iniciado\r\n");
+}
+
+double excitar_iones(int valor) {
+    double determinante = (double)valor;
+    double arcoc = acos(determinante > 1.0 ? 1.0 : determinante);
+    double superposicion = atan(PI) * atan(fabs(-pow(arcoc, 2)));
+    return superposicion;
 }
 
 uint8_t medir_qubit(double estado) {
@@ -31,22 +52,25 @@ uint8_t medir_qubit(double estado) {
 
 void procesar_datos() {
     for (int i = 0; i < ENTRADAS; i++) {
-        double estado = excitar_iones(entrada[i]);
-        salida[i] = medir_qubit(estado);
+        double estado = excitar_iones(entrada_digital[i]);
+        salida_digital[i] = medir_qubit(estado);
     }
 }
 
 void exportar_salida() {
-    printf("Resultado de procesamiento I/O digital:\n");
+    uart_print("Salida digital:\r\n");
     for (int i = 0; i < ENTRADAS; i++) {
-        printf("%d ", salida[i]);
+        uart_print_num(salida_digital[i]);
+        uart_transmit(' ');
     }
-    printf("\n");
+    uart_print("\r\n");
 }
 
-int main() {
+int main(void) {
+    uart_init();
     inicializar_chip();
     procesar_datos();
     exportar_salida();
+    while (1) {}
     return 0;
 }
